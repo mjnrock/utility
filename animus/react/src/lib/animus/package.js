3b97796dad6c4332c681a30ed93e1d306408fc56
Matt Nawrocki <mjnrock@umich.edu>
2019-04-29 13:17:12 -0400

    init

---
+++ animus/react/src/lib/animus/package.js
@@ -0,0 +1,9 @@
+import Cellular from "./cellular/package";
+import Hive from "./hive/package";
+import Utility from "./utility/package";
+
+export default {
+    Cellular,
+    Hive,
+    Utility
+}
