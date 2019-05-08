56f0bbfe71ee80f906a7268e86d391f75a4725dc
Matt Nawrocki <mjnrock@umich.edu>
2019-05-08 10:50:01 -0400

    init

---
+++ animus/react/src/lib/animus/utility/Parser.js
@@ -0,0 +1,31 @@
+import Animus from "./../package";
+
+class Parser {
+    static Query(query, tag) {
+        //TODO
+    }
+
+    static Find(tag, value, searchType = Animus.Quanta.Quantum.EnumAttributeType.KEY) {
+        for(let i in tag.GetValue()) {
+            try {
+                let aqt = tag.GetValue()[i],
+                    key = Animus.Quanta.Quantum.ReverseEnum("AttributeType", searchType).toLowerCase();
+                    key = key.charAt(0).toUpperCase() + key.slice(1);   // Evaluates to "Key, "Id", "Data", "Meta", etc.
+
+                if(aqt[ `Get${ key }` ]() === value) {
+                    return aqt;
+                }
+            } catch(e) {
+                console.warn(`[Operation Aborted]: Invalid @searchType(${ searchType })`);
+            }
+        }
+        
+        return false;
+    }
+
+    static Has(tag, value, searchType = Animus.Quanta.Quantum.EnumAttributeType.KEY) {
+        return Parser.Find(tag, value, searchType) instanceof Animus.Quanta.Quantum ? true : false;
+    }
+}
+
+export default Parser;
--- animus/react/src/lib/animus/utility/package.js
+++ animus/react/src/lib/animus/utility/package.js
@@ -1,9 +1,11 @@
 import Helper from "./Helper";
 import ByteBuffer from "./ByteBuffer";
 import Transformer from "./Transformer";
+import Parser from "./Parser";
 
 export default {
     Helper,
     ByteBuffer,
-    Transformer
+    Transformer,
+    Parser
 }
