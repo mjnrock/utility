be160d32e35fc157d0bea68b6b3ceea37ab7ad56
Matt Nawrocki <mjnrock@umich.edu>
2019-05-19 16:05:58 -0400

    init

--- animus/react/src/tests/Organelle-1.test.js
+++ animus/react/src/tests/Organelle-1.test.js
@@ -1,24 +1,14 @@
 import Animus from "./../lib/animus/package";
 
-// Subscription Test
-let o = new Animus.Cellular.Organelle(Animus.Cellular.Organelle.EnumProcessType.FEED,
-    (payload) => {
-        if(Array.isArray(payload)) {
-            return payload.reduce((a, c, i) => a + c);
-        }
-    },
-    (payload) => {
-        return payload * 5;
-    }
-);
+// Activation Testing
 let org = new Animus.Cellular.Organelle(
-    (payload) => {
-        console.log(payload);
-    }
+    [ payload => console.log(1) ],
+    [ "test", payload => console.log(2) ],
+    [ () => false, payload => console.log(3) ],
+    [ () => true, payload => console.log(4) ],
 );
+let z1 = new Animus.Cellular.ZetaEnzyme("cats");
+let z2 = new Animus.Cellular.ZetaEnzyme("test");
 
-o._id = "o";
-org._id = "org";
-
-o.Subscribe(org);
-o.Metabolize([ 5, 5, 3 ]);
+// org.Metabolize(z1);
+org.Metabolize(z2);
