0fc3ed7286119cfac6e647f676551ff4330aca23
Matt Nawrocki <mjnrock@umich.edu>
2019-05-19 16:46:05 -0400

    init

--- animus/react/src/lib/animus/cellular/dom/RenderCell.js
+++ animus/react/src/lib/animus/cellular/dom/RenderCell.js
@@ -1,13 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import Cellular from "./../package";
 
+//  https://reactjs.org/
 class RenderCell extends Cellular.Cell {
     constructor(html) {
         super({
-            html
+            templates: {}
         },
         Organelle.Package(
-            [ "render", payload => payload.cell.GetState().html ] 
+            //? At this point, this "render" will invoke the ReactDOM.render() method to re-render its content
+            [ "render", () => {
+                //! Reread about Fragments: https://reactjs.org/docs/react-api.html#reactfragment, https://reactjs.org/blog/2017/11/28/react-v16.2.0-fragment-support.html#keyed-fragments
+                //TODO Rework this example to fit this Cell
+                // ReactDOM.render(
+                //     <HelloMessage name="Taylor" />,
+                //     document.getElementById('hello-example')
+                // );
+
+                // //* will JS translate into this:
+
+                //! createElement documentation: https://reactjs.org/docs/react-api.html#createelement
+                // // @HelloMessage was declared as the Class so it existed by here in example
+                // ReactDOM.render(React.createElement(HelloMessage, { name: "Taylor" }), document.getElementById('hello-example'))
+            }]
         ));
+
+        //TODO Behaviors should be turned into AQT tags and this._actions should be an AQT root tag.
+        //? TODO-change allows for Behaviors to be translated into AQT transformations
+        this.Learn("class-add", (name, reactClassObj) => {
+            this.GetState().templates[name] = React.createClass(reactClassObj);
+        });
+        this.Learn("class-remove", (name) => {
+            delete this.GetState().templates[name];
+        });
+        //TODO Pass the render invocation to the actual React renderer
+        //TODO 
     }
 }
 
